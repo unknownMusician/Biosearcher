eda6288196f05158fbb48ab2404f6317b2efe7fe
MobikTech <62842262+MobikTech@users.noreply.github.com>
2021-07-21 17:49:28 +0300

    created plant spawner

---
+++ Assets/Resources/LandManagement/PlantSpawner/Scripts/PlantsSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Biosearcher.LandManagement
+{
+    public class PlantsSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _plantPrefabs;
+        private static PlantsSpawner s_spawner;
+
+        private List<GameObject> t_plants;
+
+        private void Awake()
+        {
+            s_spawner = this;
+
+            t_plants = (List<GameObject>)Spawn(new List<Ray> {
+                new Ray(new Vector3(0, 0, 0), new Vector3(100, 100, 100))
+            });
+            //Dispose(t_plants);
+        }
+
+        public static IEnumerable<GameObject> Spawn(IEnumerable<Ray> groundNormals)
+        {
+            List<GameObject> plants = new List<GameObject>();
+            foreach (Ray ray in groundNormals)
+            {
+                plants.Add(Instantiate(s_spawner._plantPrefabs[0], ray.origin, Quaternion.FromToRotation(Vector3.up, ray.direction), s_spawner.transform));
+            }
+            return plants;
+        }
+
+        public static void Dispose(IEnumerable<GameObject> spawnedObjects)
+        {
+            foreach (GameObject plant in spawnedObjects)
+            {
+                Destroy(plant);
+            }
+        }
+    }
+}
