ca1fe90c94e85a6623f5d05c5fc2eeabad133f3e
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-09-14 21:47:44 +0300

    greenHouse fin

--- Assets/Plants/Scripts/GreenHouse.cs
+++ Assets/Plants/Scripts/GreenHouse.cs
@@ -1,20 +1,17 @@
 ﻿using Biosearcher.Level;
 using Biosearcher.Player.Interactions;
-using Biosearcher.Refactoring;
-using System;
-using System.Collections;
 using UnityEngine;
 
 namespace Biosearcher.Plants
 {
-    public class GreenHouse : MonoBehaviour, IInsertFriendly<ISeed>
+    public class GreenHouse : MonoBehaviour, IInsertFriendly<Seed>
     {
         [SerializeField] private Vector3 _alignLocalPosition;
         [SerializeField] private Score _score;
 
         private Plant _plant;
 
-        public bool TryAlign(ISeed seed)
+        public bool TryAlign(Seed seed)
         {
             if (_plant == null)
             {
@@ -24,14 +21,16 @@ namespace Biosearcher.Plants
             return false;
         }
 
-        public bool TryInsert(ISeed seed)
+        public bool TryInsert(Seed seed)
         {
             if (_plant != null)
             {
                 return false;
             }
 
-            GameObject plantObject = Instantiate(seed.PlantSettings.PlantPrefab);
+            GameObject plantObject = Instantiate((GameObject)seed.PlantSettings.PlantPrefab);
+
+            Destroy(seed.gameObject);
 
             HandleInsertTransform(plantObject);
             HandleInsertPlant(plantObject, seed.PlantSettings);
@@ -50,7 +49,7 @@ namespace Biosearcher.Plants
             plantObject.transform.SetParent(transform);
         }
 
-        private void HandleInsertPlant(GameObject plantObject, IPlantSettings plantSettings)
+        private void HandleInsertPlant(GameObject plantObject, PlantSettings plantSettings)
         {
             _plant = plantObject.GetComponent<Plant>();
             _plant.OnGrowEnd += HandlePlantGrowed;
@@ -63,59 +62,5 @@ namespace Biosearcher.Plants
 
             Destroy(_plant.gameObject);
         }
-    }
-
-    public sealed class Plant : MonoBehaviour
-    {
-        public event Action OnGrowEnd;
-
-        private bool _isGrowing = false;
-        public IPlantSettings PlantSettings { get; private set; }
-
-        public void TryStartGrow(IPlantSettings plantSettings)
-        {
-            if (!_isGrowing)
-            {
-                PlantSettings = plantSettings;
-                StartCoroutine(Growing());
-            }
-        }
-
-        [NeedsRefactor("Get growTime from plantSettings")]
-        private IEnumerator Growing()
-        {
-            _isGrowing = true;
-            float oneOverGrowTime = 1.0f / PlantSettings.GrowTime;
-            float lerp = 0.0f;
-
-            while (lerp < 1.0f)
-            {
-                HandleGrowth(lerp);
-
-                lerp += Time.deltaTime * oneOverGrowTime;
-                yield return null;
-            }
-
-            HandleGrowth(1.0f);
-
-            _isGrowing = false;
-
-            OnGrowEnd?.Invoke();
-        }
-
-        private void HandleGrowth(float lerp) => transform.localScale = lerp * Vector3.one;
-    }
-
-    public interface IPlantSettings
-    {
-        float GrowTime { get; }
-        GameObject PlantPrefab { get; }
-        int Score { get; }
-    }
-
-    public interface ISeed : IInsertable
-    {
-        IPlantSettings PlantSettings { get; }
-        Transform transform { get; }
     }
 }
--- Assets/Plants/Scripts/PlantSettings.cs
+++ Assets/Plants/Scripts/PlantSettings.cs
@@ -5,12 +5,14 @@ namespace Biosearcher.Plants
     [CreateAssetMenu(fileName = "Default Plant", menuName = "Plant Settings")]
     public class PlantSettings : ScriptableObject
     {
-        [SerializeField] private string _name;
+        [SerializeField] private string _name = "Plant";
         [SerializeField] private GameObject _plantPrefab;
-        [SerializeField] private float _growthTime;          // in seconds
+        [SerializeField, Tooltip("In seconds.")] private float _growthTime = 10.0f;          // in seconds
+        [SerializeField] private int _score = 10;          // in seconds
 
         public string Name => _name;
         public GameObject PlantPrefab => _plantPrefab;
         public float GrowthTime => _growthTime;
+        public int Score => _score;
     }
 }
