ca4599a0d5df4b6745c3beb021a2797605fc08e5
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-09-28 17:38:15 +0300

    Models + materials

---
+++ Assets/Level/Scripts/WallMaterialTracker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Biosearcher.Level
+{
+    [ExecuteAlways]
+    public sealed class WallMaterialTracker : MonoBehaviour
+    {
+        [SerializeField] private Material _material;
+
+        private void Update() => _material.SetVector("_PlayerPosition", transform.position);
+    }
+}
